36cc5d209c88ca10511ac1a88f79e166bb1b2054
PeeralMalokhani <91820725+PeeralMalokhani@users.noreply.github.com>
2022-10-12 22:59:37 +0500

    Update index.js
    I have entered the second way of the character counter without a loop

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ for (let index = 0; index < message.length; index++){
      totalChar--;
 }
 alert("you have entered : "+charSum+"  remaining characters are : "+totalChar)
+
+// without loop
+
+// let message=prompt("write your message");
+// let charSum=message.length;
+// console.log("you have entered the charcacters "+charSum+" remaining charcacters are "+(200-charSum));
