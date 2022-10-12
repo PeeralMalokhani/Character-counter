8053c1dcff191168d06de08995559cd4d5d2a07d
PeeralMalokhani <91820725+PeeralMalokhani@users.noreply.github.com>
2022-10-12 22:42:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let message="stringhellohow are you dear";
+let charSum=0;
+let totalChar=200;
+message.length;
+for (let index = 0; index < message.length; index++){
+    charSum++;
+     totalChar--;
+}
+alert("you have entered : "+charSum+"  remaining characters are : "+totalChar)
